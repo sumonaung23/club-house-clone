dd38a4a300233c7a5abfa0fc017c0daadd44923a
sumonaung23 <sumonaung575@gmail.com>
2023-12-01 23:55:38 +0630

    added infos

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -6,9 +6,14 @@ const Welcome = () => {
         <>
             <div className={style.WelcomeContainer}>
                 <h1>Welcome Page!</h1>
+                <div className={style.WelcomeInfo}>
+                <p>
+                    We're working hard to get clubhouse ready for everyone! While we wrap up the finishing touches, we're adding people gradually to make sure nothing breaks.
+                </p>
+                <p>
+                    Anyone can join with and invite from an existing user - or reserve your username and we'll text you if you have a friend on the app who can let you in. We are so greateful you're here and can't wait to have you join!
+                </p>
             </div>
-            <div className={style.WelcomeInfo}>
-                <p>We're working hard to get clubhouse ready for everyone! While we wrap up the finishing touches, we're adding people gradually.</p>
             </div>
         </>
     )
