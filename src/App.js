1b1b131a174ede665f5c915f42ce97bfc0e6cea8
sumonaung23 <sumonaung575@gmail.com>
2023-12-13 21:01:04 +0630

    added Route for CodeFirm and AllowNotification elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import AllowNotification from './pages/AllowNotification';
+import CodeConfirm from './pages/CodeConfirm';
 import PlanLayout from './pages/Layouts/PlanLayout';
 import PhoneConfirmation from './pages/PhoneConfirmation';
 import Welcome from './pages/Welcome';
@@ -12,6 +14,8 @@ function App() {
           <Routes>
             <Route path='/' element={<Welcome />} />
             <Route path='/invite' element={<PhoneConfirmation />} />
+            <Route path='/code_confirm' element={<CodeConfirm />} />
+            <Route path='/allow_notification' element={<AllowNotification />} />
           </Routes>
         </PlanLayout>
       </BrowserRouter>
