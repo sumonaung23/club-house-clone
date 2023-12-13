0afc19ff700da684a592e70d7418f51613f89a3a
sumonaung23 <sumonaung575@gmail.com>
2023-12-13 20:54:38 +0630

    added Links

--- src/pages/PhoneConfirmation.js
+++ src/pages/PhoneConfirmation.js
@@ -9,9 +9,15 @@ const PhoneConfirmation = () => {
 
     return (
         <div className={style.phoneConfirmContainer}>
+            <Link exact to='/' className={style.backBtn}>
+                <img src='/images/arrow.png' alt='' />
+            </Link>
             <h1>Enter your phone</h1>
-            <PhoneInput international defaultCountry='US' value={value} />
+            <PhoneInput international defaultCountry='US' value={value} onChange={setValue} />
             <p>By entering your number, you're aggreing to our <span>Terms of Service and Privacy Policy.</span> Thanks! </p>
+            <Link exact to='/code_confirm' className='primaryBtn d-flex align-items-centers'>
+                Next <img src='/images/nextArrowIcon.svg' alt='' className='ml' />
+            </Link>
         </div>
     )
 }
